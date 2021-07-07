dad4357537c254ff8769fde202e10aee76ec4ec7
ohletamyleta <ohletamyleta@gmail.com>
2021-07-07 16:33:44 -0400

    buildout Oauth with google

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,63 +1,63 @@
 import React, { Component } from 'react';
-import './GoogleAuth.css';
+// import './GoogleAuth.css';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-    class GoogleAuth extends Component {
-      state = { isSignedIn: null };
+class GoogleAuth extends Component {
+  state = { isSignedIn: null };
 
-      componentDidMount() {
-        window.gapi.load('client:auth2', () => {
-          window.gapi.client
-            .init({
-              clientId: `${API_KEY}`,
-              scope: 'email',
-            })
-            .then(() => {
-              this.auth = window.gapi.auth2.getAuthInstance();
-              this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-              this.auth.isSignedIn.listen(this.onAuthChange);
-            });
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId: `${API_KEY}`,
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
-      }
+    });
+  }
 
-      onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-      };
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
-      onSignInClick = () => {
-        this.auth.signIn();
-      };
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
 
-      onSignOutClick = () => {
-        this.auth.signOut();
-      };
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
 
-      renderAuthButton() {
-        if (this.state.isSignedIn === null) {
-          return null;
-        } else if (this.state.isSignedIn) {
-          return (
-            <button
-              className='loginBtn loginBtn--google'
-              onClick={this.onSignOutClick}>
-              Log Out
-            </button>
-          );
-        } else {
-          return (
-            <button
-              className='loginBtn loginBtn--google'
-              onClick={this.onSignInClick}>
-              Login With Google
-            </button>
-          );
-        }
-      }
-
-      render() {
-        return <div>{this.renderAuthButton()}</div>;
-      }
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button
+          className='loginBtn loginBtn--google'
+          onClick={this.onSignOutClick}>
+          Log Out
+        </button>
+      );
+    } else {
+      return (
+        <button
+          className='loginBtn loginBtn--google'
+          onClick={this.onSignInClick}>
+          Login With Google
+        </button>
+      );
     }
+  }
 
-    export default GoogleAuth;
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import GoogleAuth from './GoogleAuth';
 
-    const Header = () => {
-        return (
-          <div className='headerWrapper'>
-            <div className='headerTitle'>
-              <Link to='/'>StreamIt</Link>
-            </div>
-            <div className='otherLinks'>
-              <Link to='/'>All Streams</Link>
-              <GoogleAuth />
-            </div>
-          </div>
-        );
-    }
+const Header = () => {
+  return (
+    <div className='headerWrapper'>
+      <div className='headerTitle'>
+        <Link to='/'>StreamIt</Link>
+      </div>
+      <div className='otherLinks'>
+        <Link to='/'>All Streams</Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+};
 
-    export default Header;
+export default Header;
