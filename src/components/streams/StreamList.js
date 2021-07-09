ab79e669f5650656b169f4683645c465b8ced39e
ohletamyleta <ohletamyleta@gmail.com>
2021-07-09 15:41:31 -0400

    build and style list page

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,37 @@
-   import React from 'react';
+    import React from 'react';
+    import { connect } from 'react-redux';
+    import { fetchStreams } from '../../actions';
+    import './StreamList.css';
 
-    const StreamList = () => {
-        return (
+    class StreamList extends React.Component {
+        componentDidMount() {
+            this.props.fetchStreams();
+        }
+
+         renderList() {
+            return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                <div className="content">{stream.title}</div>
+                <div className="description">{stream.description}</div>
+                <hr />
+                </div>
+            )
+            })
+        }
+
+         render() {
+            return (
             <div>
-            StreamList
+                <h2 className="header">Streams</h2>
+                <div className="list">{this.renderList()}</div>
             </div>
-        )
+            )
+        }
     }
 
-    export default StreamList;
+    const mapStateToProps = (state) => {
+        return { streams: Object.values(state.streams) };
+    }
+
+    export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
     export default (state={}, action) => {
         switch(action.type) {
             case FETCH_STREAMS:
-                return { ...state, ..._.mapKeys(action.payload, 'id')};**
+                return { ...state, ..._.mapKeys(action.payload, 'id')};
             case FETCH_STREAM:
                 return { ...state, [action.payload.id]: action.payload }
             case CREATE_STREAM:
@@ -13,7 +13,7 @@ import _ from 'lodash';
             case EDIT_STREAM:
                 return { ...state, [action.payload.id]: action.payload }
             case DELETE_STREAM:
-                return _.omit(state, action.payload);**
+                return _.omit(state, action.payload);
             default:
                 return state;
         }
